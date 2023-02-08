d9d0c2b582420cdae57cadbb37aa19a217683774
woodb58 <woodb58@gmail.com>
2023-02-08 13:53:48 -0600

    added auth requirment

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 
-const { handleErrors } = require("./middlewares");
+const { handleErrors, requireAuth } = require("./middlewares");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const productIndexTemplate = require("../../views/admin/products/index");
@@ -10,17 +10,18 @@ const { requireTitle, requirePrice } = require("./validators");
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get("/admin/products", async (req, res) => {
+router.get("/admin/products", requireAuth, async (req, res) => {
   const products = await productsRepo.getAll();
   res.send(productIndexTemplate({ products }));
 });
 
-router.get("/admin/products/new", (req, res) => {
+router.get("/admin/products/new", requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
 router.post(
   "/admin/products/new",
+  requireAuth,
   upload.single("image"),
   [requireTitle, requirePrice],
   handleErrors(productsNewTemplate),
@@ -29,7 +30,7 @@ router.post(
     const { title, price } = req.body;
     await productsRepo.create({ title, price, image });
 
-    res.redirect('/admin/products')
+    res.redirect("/admin/products");
   }
 );
 
