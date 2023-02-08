5e5d54ef72add12d9aece988544f3761e832c0e3
woodb58 <woodb58@gmail.com>
2023-02-08 13:38:40 -0600

    products list

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
 
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -21,25 +21,20 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     const user = await usersRepo.create({ email, password });
 
     req.session.userId = user.id;
 
-    res.send("account created");
+    res.redirect('admin/products');
   }
 );
 
 router.get("/signout", (req, res) => {
   req.session = null;
-  res.send("Logged Out");
+  res.redirect('/signin');
 });
 
 router.get("/signin", (req, res) => {
@@ -49,20 +44,15 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    
-    if(!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }))
-    }
-
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
 
-    res.send("You have successfully signed in");
+    res.redirect('admin/products');
   }
 );
 
