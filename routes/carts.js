01ddb7a3e6442356e07be88b09c935c18cf6a3ca
woodb58 <woodb58@gmail.com>
2023-02-15 14:13:28 -0600

    logic to create cart for user and add products to cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const cartsRepo = require("../repositories/carts");
 
 const router = express.Router();
 
-router.post("/cart/products", (req, res) => {
-  console.log(req.body.productId);
+router.post("/cart/products", async (req, res) => {
+  let cart;
+  if (!req.session.cartId) {
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
 
-  res.send("added to cart")
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
+
+  res.send("added to cart");
 });
 
 module.exports = router;
