ae577f7c2f43b22576f3a14131cdf88099063cee
woodb58 <woodb58@gmail.com>
2023-02-16 15:19:51 -0600

    delete items from cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
 const productsRepo = require("../repositories/products");
-const cartShowTemplate = require('../views/carts/show')
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
@@ -41,7 +41,18 @@ router.get("/cart", async (req, res) => {
     item.product = product;
   }
 
-  res.send(cartShowTemplate({ items: cart.items }))
+  res.send(cartShowTemplate({ items: cart.items }));
+});
+
+router.post("/cart/products/delete", async (req, res) => {
+  const { itemId } = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => item.id !== itemId);
+
+  await cartsRepo.update(req.session.cartId, { items });
+
+  res.redirect("/cart");
 });
 
 module.exports = router;
