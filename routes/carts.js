af76e520c984dae3ffbf191047b456fdeebacc7e
woodb58 <woodb58@gmail.com>
2023-02-16 12:14:49 -0600

    rendering products in cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require('../views/carts/show')
 
 const router = express.Router();
 
@@ -25,6 +27,21 @@ router.post("/cart/products", async (req, res) => {
   });
 
   res.send("added to cart");
+});
+
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }))
 });
 
 module.exports = router;
